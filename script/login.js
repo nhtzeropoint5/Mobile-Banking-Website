f85fbd6f87d967cf274a23b3934171be72bf9523
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-02-24 13:26:39 +0600

    login page functionality done

--- script/login.js
+++ script/login.js
@@ -1,1 +1,17 @@
-console.log('jora lagse')
+document.getElementById('login-btn').addEventListener('click', function () {
+    const x = document.getElementById('mob-num').value;
+    const y = document.getElementById('pin').value;
+
+    
+
+    if(x == "01712501382" && y == '1234'){
+        console.log(x,y);
+        window.alert("login successful");
+        window.location.assign("/home.html");
+    }
+    else
+    {
+        // window.alert("wrong credentials");
+        // return;
+    }
+    })
