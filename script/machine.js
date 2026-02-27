c0edceef266790a340fd5e8ec8fd742f0efaa5af
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-02-27 15:09:05 +0600

    hidden done

--- script/machine.js
+++ script/machine.js
@@ -2,7 +2,7 @@
 function getValueFromInput(id){
 const input = document.getElementById(id);
 const value = input.value;
-console.log(value);
+return value;
 }
 
 function getBalance(){
@@ -13,4 +13,12 @@ function getBalance(){
 function setBalance(value){
     const balanceElement = document.getElementById("bal");
     balanceElement.innerText = value;
+}
+
+function showOnly(id){
+    const AddM = document.getElementById('add-money');
+    const CashO = document.getElementById('cash-out');
+    AddM.classList.add('hidden');
+    CashO.classList.add('hidden');
+    // document.getElementById(id).classList.remove('hidden');
 }
